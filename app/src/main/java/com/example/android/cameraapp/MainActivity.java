2d54d39e560335c5402d16c12bab97708f37fa97
Michael Habib <michaelhabib1868@gmail.com>
2017-02-07 23:17:12 -0800

    First Attempt at Camera Fix

--- app/src/main/java/com/example/android/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/android/cameraapp/MainActivity.java
@@ -1,33 +1,20 @@
 package com.example.android.cameraapp;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
-
-import java.security.Permission;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static android.R.attr.name;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button cameraIntentButton;
 
     static final int PERMISSION_REQUEST_CODE = 100;
     boolean flag;
@@ -35,69 +22,78 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        getCameraPermission();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraIntentButton = (Button) findViewById(R.id.button_view);
+
+        cameraIntentButton.setOnClickListener(cameraButtonListener);
     }
 
-    public void getCameraPermission(){
-        if (!checkPermission()) {
-            requestPermission();
+    public View.OnClickListener cameraButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            checkPermission();
+
         }
-    }
+    };
 
-    private boolean checkPermission(){
-        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
 
-        if (result == PackageManager.PERMISSION_GRANTED){
-            return true;
-        } else {
-            return false;
+    private void checkPermission() {
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.CAMERA)) {
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        PERMISSION_REQUEST_CODE);
+
+
+            }
         }
+
     }
 
-    private void requestPermission(){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA)){
-
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
-        } else {
-
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
-        }
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(MainActivity.this,"Permission granted",Toast.LENGTH_SHORT).show();
-                    //store permission in shared pref
-                    flag = true;
-                }
-                else {
-                    flag = false;
-                    Toast.makeText(MainActivity.this,"Permission denied",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_SHORT).show();
+
+                    startCamera();
+
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
                     //store permission in shared pref
                 }
                 break;
-        }
-    }
 
-    public void camera(View view){
-
-        if(flag == true) {
-            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-            startActivity(intent);
-        }
-        else{
-            Toast.makeText(MainActivity.this,"Please Grant Permission",Toast.LENGTH_LONG).show();
         }
     }
 
 
+    private void startCamera() {
+        Intent picIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(picIntent);
+    }
 
 }
+
+
 
 /*static final int REQUEST_IMAGE_CAPTURE = 1;
     String mCurrentPhotoPath;
